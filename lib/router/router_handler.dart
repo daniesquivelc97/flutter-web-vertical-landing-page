39ecd3fabe6b8492134870c3b37ba5f8a93d77c4
daniesquivelc97 <dani_esquivelc@hotmail.com>
2022-08-03 16:57:16 -0500

    Landing navigation in my web app

---
+++ lib/router/router_handler.dart
@@ -0,0 +1,16 @@
+import 'package:fluro/fluro.dart';
+import 'package:provider/provider.dart';
+import 'package:vertical_landing_page/provider/page_provider.dart';
+import 'package:vertical_landing_page/ui/pages/home_page.dart';
+
+final homeHandler = Handler(
+  handlerFunc: (context, parameters) {
+    final page = parameters['page']!.first;
+    if (page != '/') {
+      final pageProvider = Provider.of<PageProvider>(context!, listen: false);
+      pageProvider.createScrollController(page);
+      return const HomePage();
+    }
+    return null;
+  },
+);
